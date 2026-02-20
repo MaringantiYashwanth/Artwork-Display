5828828b8a6634f60c73c198e6e7b77b66d75936
MaringantiYashwanth <yashwanthmaringanti@gmail.com>
2026-02-20 13:53:33 +0530

    configured axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+async function api() {
+  try {
+    const response = await axios.get(
+      "https://api.artic.edu/api/v1/artworks?page=1",
+    );
+  } catch (error) {
+    console.log("Failed to fetch the data", error);
+  }
+}
